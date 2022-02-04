7279fbb37d8cec47496e51ca580e23194b2a8ca3
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-04 15:21:30 -0800

    commit tree 20b964f0812503b0aab2ac4287e761baef0e6760

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+//code your solution here
+
+const array = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+// function superbowlWin(array) {
+//   array.forEach(function (el) {
+//     if (el.result == "W") {
+//       console.log(el.year);
+//     }
+//   });
+// }
+
+function superbowlWin(arr) {
+  const win = arr.find((res) => {
+    return res.result == "W";
+  });
+  if (win == undefined) {
+    return win;
+  } else {
+    return win.year;
+  }
+}
